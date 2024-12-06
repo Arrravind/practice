08b7b2134789eea7a66934c03850ca1d8a9fe504
Arrravind <aravindking030@gmail.com>
2024-12-06 15:28:59 +0530

    indentation

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,23 @@ import React from 'react'
 import 'D:/Works/React JS/practice/src/css/App.css'
 
 function App(props) {
-  return (
-    <div class="container">
-      <div class="card">
-        <div id="title">{props.title}</div>
-        <div>
-          <img src={props.img} alt=''></img>
+    return (
+        <div class="container">
+            <div class="card">
+                <div id="title">{props.title}</div>
+                <div>
+                    <img src={props.img} alt=''></img>
+                </div>
+                <div>
+                    <ul>
+                        <li class="bi bi-heart">{props.likes}</li>
+                        <li class="bi bi-chat">{props.comments}</li>
+                        <li class="bi bi-send">{props.shares}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div>
-          <ul>
-            <li class="bi bi-heart">{props.likes}</li>
-            <li class="bi bi-chat">{props.comments}</li>
-            <li class="bi bi-send">{props.shares}</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
