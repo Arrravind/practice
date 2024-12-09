46f56a49cda786d736bc7e6fceb74a404fdb7b8a
Arrravind <aravindking030@gmail.com>
2024-12-09 19:54:57 +0530

    ind2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Blog from './components/Blog';
+import Calculator from './components/Calculator'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <Blog />
+    <Calculator />
   </div>
 );
 
